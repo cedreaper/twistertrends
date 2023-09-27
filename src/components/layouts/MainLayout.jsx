08e8828520f378d042d09fd06568eb52941e33c7
cedreaper <88981393+cedreaper@users.noreply.github.com>
2023-09-27 08:01:41 -0500

    [feature] Added nav bar and routing to main layout

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import {Link, NavLink } from 'react-router-dom';
 import '../../App.css';
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="layout">
+        <div className='layout'>
             <header>
-                
+            <nav className='navigation'>
+                <ul>
+                <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+                <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+                </ul>
+            </nav>
             </header>
-
             <main>
                 {children} 
             </main>
-
-            <footer>
-                
+            <footer className='layout-footer'>
+            <div className='footer-content'>
+                    <p>&copy; 2023 Cedric Green All rights reserved</p>
+                    <p><Link to='/terms'>Terms of Service</Link> | <Link to='/privacy'>Privacy Policy</Link></p>
+                </div>
             </footer>
         </div>
     );
