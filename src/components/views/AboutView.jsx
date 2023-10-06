aba63114a996f1ac63990638ce6502eecb0b91a2
cedreaper <88981393+cedreaper@users.noreply.github.com>
2023-10-05 19:41:11 -0500

    [improvement] Update title and about view

--- src/components/views/AboutView.jsx
+++ src/components/views/AboutView.jsx
@@ -4,7 +4,8 @@ const AboutView = () => {
         <div>
             <h2>About</h2>
             <p>
-                <h3>This project was born from two events:</h3>
+                <h3>This project was born from two events</h3>
+                <hr />
                 <ul>
                     <li>Being a resident of the state of Oklahoma and growing up around the natural phenomena
                         known as Tornados </li>
@@ -13,8 +14,20 @@ const AboutView = () => {
                         what we've learned throughout the course.
                     </li>
                 </ul>
+
                 The resulting project was both challenging and rewarding. I'd like to give a special thanks to Dr. Denton
                 for the initial topic feedback which started the process into this journey.
+                <br />
+                <hr />
+                <br />
+                Now for the concrete details...
+                <br />
+                <br />
+                The main focus of this web application is to research the area of annual climate change based on data from the NOAA and historical tornadic
+                data from the National Weather Service in an attempt to locate a correlation between the two events trends.
+                <br />
+                <br />
+                The data will processed through ETL principles to ensure relevancy and eliminate waste.
             </p>
         </div>
         
