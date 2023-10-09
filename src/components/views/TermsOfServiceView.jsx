c9a40599b97963a90259f5c1acf057db6fb9a2a6
cedreaper <88981393+cedreaper@users.noreply.github.com>
2023-10-08 20:02:57 -0500

    [bugfix] avoid overflow for prototype

--- src/components/views/TermsOfServiceView.jsx
+++ src/components/views/TermsOfServiceView.jsx
@@ -11,16 +11,6 @@ const TermsOfServiceView = () => {
 
             <h3>3. Data Accuracy and Integrity</h3>
             <p>While TwisterTrends endeavors to provide accurate and current data, we cannot guarantee the completeness, timeliness, or accuracy of the data presented. Data may be subject to errors, inaccuracies, or may not be up-to-date at the moment of access.</p>
-
-            <h3>4. Limitation of Liability <span style={{color : "yellow"}}>(Scroll down for more terms)</span></h3>
-            <p>In no event shall TwisterTrends, its operators, affiliates, or any third parties involved in creating, producing, or delivering the website be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of your access to, use of, or inability to use the website, even if TwisterTrends has been advised of the possibility of such damages. This limitation includes damages or any viruses which may affect your computer equipment.</p>
-
-            <h3>5. No Warranties</h3>
-            <p>All content on the TwisterTrends website is provided to you "as is" without warranty of any kind, either express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, or non-infringement.</p>
-
-            <h3>6. Modification of Terms</h3>
-            <p>TwisterTrends reserves the right to modify or update these Terms of Service at any time without prior notice. Your continued usage of the TwisterTrends website will mean you accept those changes.</p>
-
         </div>
     );
 }
